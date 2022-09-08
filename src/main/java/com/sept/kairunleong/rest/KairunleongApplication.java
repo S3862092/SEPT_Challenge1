845bbc5ee1fd6562b93442aa957427cae15ee3b1
Kai Run Leong <s3862092@student.rmit.edu.au>
2022-09-09 09:14:37 +1000

    initial commit

---
+++ src/main/java/com/sept/kairunleong/rest/KairunleongApplication.java
@@ -0,0 +1,13 @@
+package com.sept.kairunleong.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KairunleongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KairunleongApplication.class, args);
+    }
+
+}
