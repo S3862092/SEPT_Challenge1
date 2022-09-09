8358f08150408d1e1527afe11d83ac9e63d17c5f
Kai Run Leong <s3862092@student.rmit.edu.au>
2022-09-09 10:52:06 +1000

    Will have to checkout to item_dao branch to fix a static issue

--- src/main/java/com/sept/kairunleong/rest/controller/ItemController.java
+++ src/main/java/com/sept/kairunleong/rest/controller/ItemController.java
@@ -1,4 +1,28 @@
 package com.sept.kairunleong.rest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.sept.kairunleong.rest.model.Item;
+import com.sept.kairunleong.rest.model.Items;
+import com.sept.kairunleong.rest.dao.ItemDAO;
+
+@RestController
+@RequestMapping(path = "/items")
 public class ItemController {
+    @Autowired
+    private ItemDAO itemDao;
+
+    @GetMapping(path="/", produces = "application/json")
+    public Items getEmployees()
+    {
+        return ItemDAO.getAllItems();
+    }
 }
