d4de561c4b34da0632f3e83267195b58687a02a4
Kai Run Leong <s3862092@student.rmit.edu.au>
2022-09-09 15:00:46 +1000

    created dummy exception class (NOT MARKED FOR SUBMISSION)

---
+++ src/main/java/com/sept/kairunleong/rest/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.sept.kairunleong.rest.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class CustomException {
+    //dummy class
+}
